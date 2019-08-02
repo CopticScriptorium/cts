f22bd4972112bbd30d15728d24d0512f29ffacad
Luke Gessler <lukegessler@gmail.com>
2019-08-02 13:29:56 -0400

    fix empty template dir

--- coptic/coptic/settings/base.py
+++ coptic/coptic/settings/base.py
@@ -51,11 +51,10 @@ MIDDLEWARE = MIDDLEWARE_CLASSES # for django >= 1.10
 TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': [],
+		'DIRS': [os.path.join(BASE_DIR, 'templates')],
 		'APP_DIRS': True,
 		'OPTIONS': {
 			'context_processors': [
-				"django.core.context_processors.request",
 				"django.contrib.auth.context_processors.auth",
 				"django.contrib.messages.context_processors.messages"
 			]
