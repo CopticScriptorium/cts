9ec6781f5eef4f5f4dd8e4bbb844b096ee66c51c
Ori Pekelman <ori@pekelman.com>
2025-01-17 12:45:31 +0100

    cleanup before import

--- coptic/addcorpora.sh
+++ coptic/addcorpora.sh
@@ -5,4 +5,7 @@ if [ -z "$1" ]; then
     exit 1
 fi
 CORPORA="acts-pilate abraham AP besa-letters bohairic-habakkuk bohairic-life-isaac bohairic.1corinthians bohairic.mark bohairic.nt bohairic.ot book-bartholomew doc-papyri dormition-john helias johannes-canons john-constantinople lament-mary life-aphou life-cyrus life-eustathius-theopiste life-john-kalybites life-longinus-lucius life-onnophrius life-paul-tamma life-phib life-pisentius magical-papyri martyrdom-victor mercurius mysteries-john pachomius-instructions pistis-sophia proclus-homilies pseudo-athanasius-discourses pseudo-basil pseudo-celestinus pseudo-chrysostom pseudo-ephrem pseudo-flavianus pseudo-theophilus pseudo-timothy sahidic.ot sahidic.ruth sahidica.1corinthians sahidica.mark sahidica.nt shenoute-a22 shenoute-considering shenoute-crushed shenoute-dirt shenoute-eagerness shenoute-errs shenoute-fox shenoute-house shenoute-listen shenoute-night shenoute-place shenoute-prince shenoute-seeks shenoute-those shenoute-thundered shenoute-true shenoute-uncertain-xr shenoute-unknown5_1 shenoute-witness theodosius-alexandria"
-python manage.py addcorpus --local-repo-path=$1 $CORPORA
+./manage.py delete_index
+./manage.py clearcache
+./manage.py migrate
+./manage.py addcorpus --local-repo-path=$1 $CORPORA
