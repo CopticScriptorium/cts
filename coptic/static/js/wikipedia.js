743553d4417f10da987b713286eb79cde4bdb77f
Ori Pekelman <ori@pekelman.com>
2025-02-04 23:09:00 +0100

    rewrite wikipedia.js in modern style so it can be debugged. Add https support

--- coptic/static/js/wikipedia.js
+++ coptic/static/js/wikipedia.js
@@ -1,83 +1,7 @@
-const WIKIPEDIA = (function() {
-  const my = {};
-
-  // DBPedia SPARQL endpoint
-  my.endpoint = 'https://dbpedia.org/sparql/';
-
-  // ### getData
-  //
-  // Return structured information (via callback) on the provided Wikipedia URL by querying
-  // the DBPedia SPARQL endpoint and then tidying the data up.
-  //
-  // @param: wikipediaUrlOrPageName. A wikipedia URL or pageName or an object
-  // with following struture:
-  //
-  //     {
-  //       url: wikipediaURLOrPageName,
-  //       raw: false // do not include the raw data in what is returned
-  //     }
-  //
-  // @return: Data is return in the form of the following hash:
-  //
-  //    {
-  //      raw: the-raw-json-from-dbpedia,
-  //      summary: a-cleaned-up-set-of-the-properties (see extractSummary),
-  //      dbpediaUrl: dbpedia-resource-url e.g. https://dbpedia.org/resource/World_War_II
-  //    }
-  //
-  // Function is asynchronous as we have to call out to DBPedia to get the
-  // info.
-  my.getData = function(wikipediaUrlOrPageName, callback, error) {
-    const url = my._getDbpediaUrl(wikipediaUrlOrPageName);
-    function onSuccess(data) {
-      const out = {
-        raw: data,
-        dbpediaUrl: url,
-        summary: null
-      };
-      if (data) {
-        out.summary = my.extractSummary(url, data);
-      } else {
-        out.error = 'Failed to retrieve data. Is the URL or page name correct?';
-      }
-      callback(out);
-    }
-    my.getRawJson(url, onSuccess, error);
-  };
-
-  // ### _getDbpediaUrl
-  //
-  // Convert the incoming URL or page name to a DBPedia url
-  my._getDbpediaUrl = function(url) {
-    if (url.indexOf('wikipedia')!=-1) {
-      const parts = url.split('/');
-      const title = parts[parts.length-1];
-      url = 'https://dbpedia.org/resource/' + title;
-      return url;
-    } else if (url.indexOf('dbpedia.org')!=-1) {
-      return url;
-    } else {
-      url = 'https://dbpedia.org/resource/' + url.replace(/ /g, '_');
-      return url;
-    }
-  };
-
-  // ### getRawJson
-  //
-  // get raw RDF JSON for DBPedia resource from DBPedia SPARQL endpoint
-  my.getRawJson = function(url, callback, error) {
-    const sparqlQuery = 'DESCRIBE <{{url}}>'.replace('{{url}}', url);
-    fetch(my.endpoint + '?' + new URLSearchParams({
-      query: sparqlQuery,
-      format: 'application/rdf+json'
-    }))
-    .then(response => response.json())
-    .then(callback)
-    .catch(error);
-  };
-
-  // Standard RDF namespace prefixes for use in lookupProperty function
-  my.PREFIX = {
+// config.js
+const CONFIG = {
+  ENDPOINT: 'https://dbpedia.org/sparql/',
+  PREFIX: {
     rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     rdfs: "http://www.w3.org/2000/01/rdf-schema#",
     xsd: "http://www.w3.org/2001/XMLSchema#",
@@ -89,32 +13,113 @@ const WIKIPEDIA = (function() {
     dbo: "http://dbpedia.org/ontology/",
     geo: "http://www.geonames.org/ontology#",
     wgs: "http://www.w3.org/2003/01/geo/wgs84_pos#"
-  };
+  }
+};
 
-  my._expandNamespacePrefix = function(uriWithPrefix) {
-    for(const key in WIKIPEDIA.PREFIX) {
+// Custom error class
+class WikipediaError extends Error {
+  constructor(message, originalError = null) {
+    super(message);
+    this.name = 'WikipediaError';
+    this.originalError = originalError;
+  }
+}
+
+// WikipediaService.js
+class WikipediaService {
+  constructor(endpoint = CONFIG.ENDPOINT) {
+    this.endpoint = endpoint;
+  }
+
+  // ### getData
+  //
+  // Return structured information on the provided Wikipedia URL by querying
+  // the DBPedia SPARQL endpoint and then tidying the data up.
+  //
+  // @param: wikipediaUrlOrPageName. A wikipedia URL or pageName or an object
+  // with following struture:
+  //
+  //     {
+  //       url: wikipediaURLOrPageName,
+  //       raw: false // do not include the raw data in what is returned
+  //     }
+  //
+  // @return: Promise that resolves to data in the form:
+  //
+  //    {
+  //      raw: the-raw-json-from-dbpedia,
+  //      summary: a-cleaned-up-set-of-the-properties (see extractSummary),
+  //      dbpediaUrl: dbpedia-resource-url e.g. http://dbpedia.org/resource/World_War_II
+  //    }
+  async getData(wikipediaUrlOrPageName) {
+    try {
+      const url = this._getDbpediaUrl(wikipediaUrlOrPageName);
+      const data = await this.getRawJson(url);
+      
+      return {
+        raw: data,
+        dbpediaUrl: url,
+        summary: data ? this.extractSummary(url, data) : null,
+        error: !data ? 'Failed to retrieve data. Is the URL or page name correct?' : null
+      };
+    } catch (err) {
+      throw new WikipediaError('Error fetching Wikipedia data', err);
+    }
+  }
+
+  // ### _getDbpediaUrl
+  //
+  // Convert the incoming URL or page name to a DBPedia url
+  _getDbpediaUrl(url) {
+    if (url.indexOf('wikipedia')!=-1) {
+      const parts = url.split('/');
+      const title = parts[parts.length-1];
+      url = 'http://dbpedia.org/resource/' + title;
+      return url;
+    } else if (url.indexOf('dbpedia.org')!=-1) {
+      return url;
+    } else {
+      url = 'http://dbpedia.org/resource/' + url.replace(/ /g, '_');
+      return url;
+    }
+  }
+
+  // ### getRawJson
+  //
+  // get raw RDF JSON for DBPedia resource from DBPedia SPARQL endpoint
+  async getRawJson(url) {
+    const sparqlQuery = 'DESCRIBE <{{url}}>'.replace('{{url}}', url);
+    try {
+      const response = await fetch(this.endpoint + '?' + new URLSearchParams({
+        query: sparqlQuery,
+        format: 'application/rdf+json'
+      }));
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      
+      return await response.json();
+    } catch (err) {
+      console.error('Error fetching RDF JSON:', err);
+      throw err;
+    }
+  }
+
+  _expandNamespacePrefix(uriWithPrefix) {
+    for(const key in CONFIG.PREFIX) {
       if (uriWithPrefix.indexOf(key + ':') === 0) {
-        uriWithPrefix = WIKIPEDIA.PREFIX[key] + uriWithPrefix.slice(key.length + 1);
+        uriWithPrefix = CONFIG.PREFIX[key] + uriWithPrefix.slice(key.length + 1);
       }
     }
     return uriWithPrefix;
-  };
+  }
 
   // ### lookupProperty
   // 
   // lookup a property value given a standard RDF/JSON property dictionary
-  // e.g. something like ...
-  // 
-  //       ...
-  //       "http://dbpedia.org/property/regent": [
-  //              {
-  //                  "type": "uri",
-  //                  "value": "http://dbpedia.org/resource/Richard_I_of_England"
-  //              }
-  //          ],
-  //       ...
-  my._lookupProperty = function(dict, property) {
-    property = my._expandNamespacePrefix(property);
+  _lookupProperty(dict, property) {
+    property = this._expandNamespacePrefix(property);
     const values = dict[property];
     for (const idx in values) {
       // only take english values if lang is present
@@ -122,96 +127,107 @@ const WIKIPEDIA = (function() {
         return values[idx].value;
       }
     }
-  };
+  }
 
-  // Extract a standard set of attributes (e.g. title, description, dates etc
-  // etc) from rdfJson and the given subject uri (url) e.g.
-  // 
-  //      extractSummary('http://dbpedia.org/resource/Rufus_Pollock', rdfJson object from dbpedia)
-  my.extractSummary = function(subjectUri, rdfJson) {
-    const properties = rdfJson[subjectUri];
-    function lkup(attribs) {
-      if (attribs instanceof Array) {
-        const out = [];
-        for (const idx in attribs) {
-          const _tmp = my._lookupProperty(properties, attribs[idx]);
-          if (_tmp) {
-            out.push(_tmp);
-          }
-        }
-        return out;
-      } else {
-        return my._lookupProperty(properties, attribs);
-      }
+  // Helper method to handle property lookups
+  _lookupProperties(properties, propertyNames) {
+    if (Array.isArray(propertyNames)) {
+      return propertyNames
+        .map(prop => this._lookupProperty(properties, prop))
+        .filter(Boolean);
     }
+    return this._lookupProperty(properties, propertyNames);
+  }
 
-    const summaryInfo = {
-      title: lkup('rdfs:label'),
-      description: lkup('dbo:abstract'),
-      summary: lkup('rdfs:comment'),
-      startDates: lkup(['dbo:birthDate', 'dbo:formationDate', 'dbo:foundingYear']),
-      endDates: lkup('dbo:deathDate'),
-      // both dbp:date and dbo:date are usually present but dbp:date is
-      // frequently "bad" (e.g. just a single integer rather than a date)
-      // whereas ontology value is better
-      date: lkup('dbo:date'),
-      place: lkup('dbp:place'),
-      birthPlace: lkup('dbo:birthPlace'),
-      deathPlace: lkup('dbo:deathPlace'),
-      source: lkup('foaf:page'),
-      images: lkup(['dbo:thumbnail', 'foaf:depiction', 'foaf:img']),
+  // Extract a standard set of attributes from rdfJson
+  extractSummary(subjectUri, rdfJson) {
+    const properties = rdfJson[subjectUri];
+    const lookup = props => this._lookupProperties(properties, props);
+
+    const summary = {
+      title: lookup('rdfs:label'),
+      description: lookup('dbo:abstract'),
+      summary: lookup('rdfs:comment'),
+      dates: {
+        start: lookup(['dbo:birthDate', 'dbo:formationDate', 'dbo:foundingYear']),
+        end: lookup('dbo:deathDate'),
+        general: lookup('dbo:date')
+      },
+      place: lookup('dbp:place'),
+      birthPlace: lookup('dbo:birthPlace'),
+      deathPlace: lookup('dbo:deathPlace'),
+      source: lookup('foaf:page'),
+      images: lookup(['dbo:thumbnail', 'foaf:depiction', 'foaf:img']),
       location: {
-        lat: lkup('wgs:lat'),
-        lon: lkup('wgs:long')
+        lat: lookup('wgs:lat'),
+        lon: lookup('wgs:long')
       },
       types: [],
       type: null
     };
 
-    // getLastPartOfUrl
     function gl(url) {
       const parts = url.split('/');
       return parts[parts.length-1];
     }
 
-    const typeUri = my._expandNamespacePrefix('rdf:type');
-    const types = [];
+    const typeUri = this._expandNamespacePrefix('rdf:type');
     const typeObjs = properties[typeUri];
     for(const idx in typeObjs) {
       const value = typeObjs[idx].value;
-      // let's be selective
-      // ignore yago and owl stuff
       if (value.indexOf('dbpedia.org/ontology') != -1 || value.indexOf('schema.org') != -1 || value.indexOf('foaf/0.1') != -1) {
-        // TODO: ensure uniqueness (do not push same thing ...)
-        summaryInfo.types.push(gl(value));
-        // use schema.org value as the default
+        summary.types.push(gl(value));
         if (value.indexOf('schema.org') != -1) {
-          summaryInfo.type = gl(value);
+          summary.type = gl(value);
         }
       }
     }
-    if (!summaryInfo.type && summaryInfo.types.length > 0) {
-      summaryInfo.type = summaryInfo.types[0];
+    if (!summary.type && summary.types.length > 0) {
+      summary.type = summary.types[0];
     }
 
-    summaryInfo.start = summaryInfo.startDates.length > 0 ? summaryInfo.startDates[0] : summaryInfo.date;
-    summaryInfo.end = summaryInfo.endDates;
-    if (!summaryInfo.place) {
-      // death place is more likely more significant than death place
-      summaryInfo.place = summaryInfo.deathPlace || summaryInfo.birthPlace;
+    summary.start = summary.dates.start.length > 0 ? summary.dates.start[0] : summary.dates.general;
+    summary.end = summary.dates.end;
+    if (!summary.place) {
+      summary.place = summary.deathPlace || summary.birthPlace;
     }
-    // if place a uri clean it up ...
-    if (summaryInfo.place) {
-      summaryInfo.place = gl(summaryInfo.place);
+    if (summary.place) {
+      summary.place = gl(summary.place);
     }
-    summaryInfo.location.title = summaryInfo.place;
-    summaryInfo.image = summaryInfo.images ? summaryInfo.images[0] : null;
+    summary.location.title = summary.place;
+    summary.image = summary.images ? summary.images[0] : null;
 
-    return summaryInfo;
-  };
+    return this._enrichSummary(summary);
+  }
 
-  // Function to display Wikipedia information
-  my.wikipop = function(e, article_title) {
+  _enrichSummary(summary) {
+    // Implementation of _enrichSummary method
+    return summary;
+  }
+}
+
+// UI Handler class
+class WikipediaPopup {
+  constructor(selector = '.wiki-link') {
+    this.wikiService = new WikipediaService();
+    this.selector = selector;
+    this.hideTimeout = null;
+    this.setupEventListeners();
+  }
+
+  setupEventListeners() {
+    document.addEventListener('DOMContentLoaded', () => {
+      document.querySelectorAll(this.selector).forEach(el => {
+        el.addEventListener('mouseover', (e) => {
+          const articleTitle = el.dataset.title;
+          this.show(e, articleTitle);
+          console.log(articleTitle);
+        });
+      });
+    });
+  }
+
+  async show(e, articleTitle) {
     const target = e.target;
     const position = {
       top: target.getBoundingClientRect().top + window.scrollY - 3,
@@ -221,16 +237,16 @@ const WIKIPEDIA = (function() {
     const infobox = document.getElementById("infobox");
     
     // Set fixed dimensions before displaying content
-    infobox.style.width = "60%"; // Set a fixed width
-    infobox.style.minWidth = "200px"; // Set a fixed width
-    infobox.style.minHeight = "300px"; // Set a max height
-    infobox.style.height = "fit-content"; // Set a max height
-    infobox.style.overflowY = "auto"; // Allow vertical scrolling if needed
+    infobox.style.width = "60%";
+    infobox.style.minWidth = "200px";
+    infobox.style.minHeight = "300px";
+    infobox.style.height = "fit-content";
+    infobox.style.overflowY = "auto";
     
     // clear box
     document.querySelector('.thumbnail').style.display = "none";
     document.querySelector('.summary').textContent = "";
-    document.querySelector('.title').innerHTML = `<h4>${article_title}</h4><span> (<a href="https://en.wikipedia.org/wiki/${article_title.replace(/ /g, "_")}" target="_blank">open in Wikipedia</a>)</span>`;
+    document.querySelector('.title').innerHTML = `<h4>${articleTitle}</h4><span> (<a href="https://en.wikipedia.org/wiki/${articleTitle.replace(/ /g, "_")}" target="_blank">open in Wikipedia</a>)</span>`;
 
     infobox.style.display = "block";
     infobox.style.top = position.top + 'px';
@@ -243,41 +259,41 @@ const WIKIPEDIA = (function() {
     // Add event listeners
     let hideTimeout;
     newInfobox.addEventListener("mouseout", function(e) {
-      // Check if the mouse is really leaving the infobox (not moving to a child element)
       if (!newInfobox.contains(e.relatedTarget)) {
-        hideTimeout = setTimeout(my.hide_wiki, 300); // Add small delay before hiding
+        hideTimeout = setTimeout(this.hide_wiki, 300);
       }
-    });
+    }.bind(this));
     
     newInfobox.addEventListener("mouseover", function() {
       if (hideTimeout) {
-        clearTimeout(hideTimeout); // Cancel hiding if mouse returns
+        clearTimeout(hideTimeout);
       }
     });
 
     document.querySelectorAll(".index-list, .meta-value, .index-annis, h1").forEach(el => {
-      el.addEventListener("mouseover", my.hide_wiki);
+      el.addEventListener("mouseover", this.hide_wiki);
     });
 
-    article_title = "https://en.wikipedia.org/wiki/" + article_title.replace(/ /g, "_");
-    document.querySelector('.summary').textContent = ""; // pre-emptively prepare missing entry response
+    articleTitle = "https://en.wikipedia.org/wiki/" + articleTitle.replace(/ /g, "_");
+    document.querySelector('.summary').textContent = "";
 
-    const display = function(info) {
+    try {
+      const info = await this.wikiService.getData(articleTitle);
+      
       if (!info) {
         document.querySelector('.summary').textContent = "[No Wikidata entry found for this entity]";
-        return true;
+        return;
       }
-      const rawData = info.raw;
-      const summaryInfo = info.summary;
-      const properties = rawData[info.dbpediaUrl];
 
-      document.querySelector('.title').innerHTML = `<h4>${summaryInfo.title}</h4><span> (<a href="${article_title}" target="_blank">open in Wikipedia</a>)</span>`;
+      const summaryInfo = info.summary;
+
+      document.querySelector('.title').innerHTML = `<h4>${summaryInfo.title}</h4><span> (<a href="${articleTitle}" target="_blank">open in Wikipedia</a>)</span>`;
 
       if (summaryInfo.summary) {
-        document.querySelector('.summary').textContent = summaryInfo["summary"];
+        document.querySelector('.summary').textContent = summaryInfo.summary;
       } else {
         document.querySelector('.summary').textContent = "[No Wikidata entry found for this entity]";
-        return true;
+        return;
       }
 
       if (summaryInfo.image) {
@@ -290,9 +306,8 @@ const WIKIPEDIA = (function() {
       map.style.display = "none";
       const special_meta = "people";
       if ('location' in summaryInfo) {
-        if ('lat' in summaryInfo["location"] && 'lon' in summaryInfo["location"]) {
-          const lon = summaryInfo["location"].lon;
-          const lat = summaryInfo["location"].lat;
+        if ('lat' in summaryInfo.location && 'lon' in summaryInfo.location) {
+          const {lon, lat} = summaryInfo.location;
           if (lon && lat && special_meta == 'places') {
             const map_url = `https://maps.google.com/maps?q=${lat},${lon}`;
             map.innerHTML = `<a href="${map_url}"><i class="fa fa-map-marker"></i> Map</a>`;
@@ -300,27 +315,21 @@ const WIKIPEDIA = (function() {
           }
         }
       }
-    };
 
-    my.getData(article_title, display, function(error) {
-      console.log(error);
-    });
-  };
+    } catch (err) {
+      console.error('Error displaying Wikipedia info:', err);
+      document.querySelector('.summary').textContent = "Error loading Wikipedia data";
+    }
+  }
 
-  my.hide_wiki = function() {
+  hide_wiki() {
     document.getElementById("infobox").style.display = "none";
-  };
+  }
+}
 
-  // Ensure the functions are called after the page content is loaded
-  document.addEventListener('DOMContentLoaded', function() {
-    // Example usage: Attach wikipop to elements with class 'wiki-link'
-    document.querySelectorAll('.wiki-link').forEach(el => {
-      el.addEventListener('mouseover', function(e) {
-        const article_title = this.dataset.title;
-        my.wikipop(e, article_title);
-      });
-    });
-  });
+// Make classes available globally
+window.WikipediaService = WikipediaService;
+window.WikipediaPopup = WikipediaPopup;
 
-  return my;
-})();
+const wikiPopup = new WikipediaPopup();
+console.log(wikiPopup);
