83d5447f73cbfcd5dd670b23bd2ec06c67c06b65
Ori Pekelman <ori@pekelman.com>
2025-02-04 20:50:00 +0100

    Remove django-grappelli dependency from project

--- coptic/coptic/settings/base.py
+++ coptic/coptic/settings/base.py
@@ -18,7 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 # Application definition
 INSTALLED_APPS = (
-    "grappelli",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
